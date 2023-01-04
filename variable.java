7b703654b95c4a8d09d02d28ad99aebe71090db5
chaitanya4455 <109501674+chaitanya4455@users.noreply.github.com>
2023-01-04 22:02:15 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+class variable {
+    public static void main(String[] args) {
+        double a = 3.14;
+        Scanner sc = new Scanner(System.in);
+        // The radius of circle b
+        double b = sc.nextInt();
+        double c= a * (b*b);
+       System.out.println(c);
+         
+    }
+}
